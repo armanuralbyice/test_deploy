23bf143e3122b2f738920f5417054f5905358eb0
armanuralbyice <armanurhossainalbyice0@gmail.com>
2024-06-21 16:14:56 +0600

    deploy

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://test-deploy-z697.vercel.app/",
+    origin: "https://test-deploy-z697.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Authorization", "Content-Type"],
